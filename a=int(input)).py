36a2b4ae9454d88fc6117d481d2748ba609535b2
ramtinsaffarkhorasani <88191906+ramtinsaffarkhorasani@users.noreply.github.com>
2021-07-31 22:10:53 +0430

    Update a=int(input)).py

--- a=int(input)).py
+++ a=int(input)).py
@@ -4,6 +4,11 @@ print("enter your height")
 b=int(input())
 c = a / (b * b) 
 print("your bmi is")
+if c>25:
+    print(jose kochak )
+if c>30:
+    print(jose motevaset)
+if c>35:
+    print(jose bozorg)
 print(c)
-
        
